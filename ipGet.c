e0814bfecc836d3bb5f7392a9244d95300cd7e5a
Matheus <59947028+MatheusHash@users.noreply.github.com>
2022-03-02 10:12:43 -0300

    Update ipGet.c

--- ipGet.c
+++ ipGet.c
@@ -1,16 +1,20 @@
+/*
+    Author: Matheus Ferreira Pimenta SilvA
+*/ 
+
 #include<stdio.h>
 #include<netdb.h>
 #include<arpa/inet.h>
 
 int main(int argc, char* argv[]){
 
-    char *alvo;
-    alvo = argv[1];
+    char *target;
+    target = argv[1];
 
     struct hostent *host;
     host = gethostbyname(alvo);
 
-    printf("%s ==> %s\n", alvo, inet_ntoa(*((struct in_addr *)host->h_addr)));
+    printf("%s ==> %s\n", target, inet_ntoa(*((struct in_addr *)host->h_addr)));
     
 return 0;
 } 
